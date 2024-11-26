6a8c1e0568c0763dc5c0f8deae0e6764a967aba8
Stavros Syrmpopoulos <87983517+sirbo44@users.noreply.github.com>
2024-11-26 15:14:56 +0200

    Add files via upload
    Connected the flights API. Now we can display basic folium.Markers for the airports and airplanes folium.Markers for the airplanes. The API is free and has no limit to the requests. Better find a new one because not all flights are displayed properly. Also, we need to change the airport CSV file because it is not updated.

--- map.py
+++ map.py
@@ -1,9 +1,17 @@
 import folium, requests
 from ip2geotools.databases.noncommercial import DbIpCity
+from folium.plugins import MousePosition
+
 
 # identify the IP address of the client
 ip = requests.get('https://api.ipify.org').text
 key = '949D49520ADEF75652B386A0317E08A8'
+displays = ['240×160','320×240','432×240','480×270','480×320','640×400','800×480','854×480','1024×576','1280×720','1280×768','1280×800','1366×768','1366×900',
+            '1440×900','1600×900','1680×945','1680×1050','1920×1080','1920×1200','2048×1152','2560×1440','2560×1600','3200×2048','3840×2160','3200×2400','3840×2400','5120×2880','5120×3200','5760×3240','6400×4096','6400×4800','7680×4320','7680×4800','15360×8640']
+
+
+
+
 
 # get the location of the client
 #
@@ -18,11 +26,11 @@ min_lat = -90
 max_lat = 90
 min_lon = -180
 max_lon = 180
-
+center = [37.97945, 23.71625]
 m = folium.Map(control_scale=True,
                 max_bounds=True,
                 max_bounds_viscosity=1.0,
-                location=[37.97945, 23.71625], # TO BE CHANGED TO [lat, lon] BEFORE RELEASE
+                location=center,#37.97945, 23.71625], # TO BE CHANGED TO [lat, lon] BEFORE RELEASE
                 zoom_start=6, 
                 min_lat=min_lat,
                 max_lat=max_lat,
@@ -30,8 +38,12 @@ m = folium.Map(control_scale=True,
                 max_lon=max_lon,
                 min_zoom=3)
 
-# flights = requests.get('https://opensky-network.org/api/states/all').json()
-# print(flights)
+flights = requests.get('https://opensky-network.org/api/states/all').json()
+flights = flights['states']
+print("X:", flights[1][5], "Y:", flights[1][6])
+print(type(flights[1][5]))
+
+
 
 f = open('airports.csv', 'r+')
 airports = f.readlines()
@@ -40,8 +52,18 @@ for i in range(len(airports)):
     airports[i][15] = airports[i][15].replace('"\n', '')
     
     if airports[i][14] != 0.000 and airports[i][15] != 0.000 and airports[i][1] != 'N/A':
-        if float(airports[i][14]) >= 32.5889 and float(airports[i][14]) <= 43.1 and float(airports[i][15]) >= 8.7425 and float(airports[i][15]) <= 38.69:
+        if float(airports[i][14]) >= 29.24806 and float(airports[i][14]) <= 44.78027 and float(airports[i][15]) >= 2.61475 and float(airports[i][15]) <= 54.81349:
             folium.Marker(location=[airports[i][14],airports[i][15]], popup=airports[i][1]).add_to(m)
+for flight in flights:
+    if flight[5] != None and flight[6] != None:
+        if float(flight[5]) >= 29.24806 and float(flight[5]) <= 44.78027 and float(flight[6]) >= 2.61475 and float(flight[6]) <= 54.81349:
+            folium.Marker(location=[flight[5],flight[6]], popup=flight[1],icon=folium.Icon(icon='plane')).add_to(m)
+# folium.Marker(location=[,], popup='test').add_to(m)
+# MousePosition().add_to(m)
+
+
+
+
 f.close()
 
 
